1a08e095e637695bd78fc65323915fdfb9325227
Stephanie Feddern <s.feddern@gmail.com>
2015-05-13 23:35:30 +0200

    added test case

--- spec/controllers/contact_forms_controller_spec.rb
+++ spec/controllers/contact_forms_controller_spec.rb
@@ -22,15 +22,30 @@ describe ContactFormsController, type: :controller do
   end
 
   describe "#create" do
-    it 'should send an email' do
-      expect{
-        post :create, user_id: user.id, name: 'Tina Tester', school: 'Penn High School', event: 'Coding is FUNtastic!', email: 'tina.tester@penn-high-school.com', message: 'Hi Biene Maya, please be our coach! Cheers, Tina', datetime: appointment
-      }.to change{ActionMailer::Base.deliveries.count}.from(0).to(1)
+    context "when input is valid" do
+      it "sends an email" do
+        expect{
+          post :create, user_id: user.id, name: 'Tina Tester', school: 'Penn High School', event: 'Coding is FUNtastic!', email: 'tina.tester@penn-high-school.com', message: 'Hi Biene Maya, please be our coach! Cheers, Tina', datetime: appointment
+        }.to change{ActionMailer::Base.deliveries.count}.from(0).to(1)
+      end
+    end
+
+    context "when input is invalid" do
+      it "renders new" do
+        post :create, user_id: user.id, email: 'tina.tester(at)test.com'
+        expect(response).to render_template(:new)
+      end
+
+      it "has error message" do
+        post :create, user_id: user.id, email: 'tina.tester(at)test.com'
+        expect(flash[:error]).to be_present
+        expect(flash[:error]).to eq('Fehler beim Versenden. Bitte prüfen Sie Ihre Angaben.')
+      end
     end
   end
 
   describe "#new" do
-    it "renders new template" do
+    it "renders new" do
       get :new, user_id: user.id
       expect(response).to render_template(:new)
     end
